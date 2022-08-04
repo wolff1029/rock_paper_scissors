38b93af475c1f003365bdc8b438ba0cc63f0c266
Austin Wolff <austin.e.wolff@gmail.com>
2022-08-04 11:10:29 -0500

    Added UI for player input and to display result

--- main.js
+++ main.js
@@ -39,15 +39,62 @@ function playRound(playerSelection, computerSelection) {
         return 'Tie!';
     }
 }
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = "rock";
-        let computerSelection = getComputerChoice();
-        console.log('playerSelection = ' + playerSelection);
-        console.log(`computerSelection = ${computerSelection}`);
-        console.log(playRound(playerSelection, computerSelection));
+function updateResult(result) {
+    let resultDiv = document.querySelector('.result')
+    if (resultDiv.textContent != 'You lose!' && resultDiv.textContent != 'You win!') {
+        resultDiv.textContent = result;
     }
+    return resultDiv;
+}
+function handleOnClick(e) {
+    //console.log(e);
+    let playerSelection = e.target.innerHTML;
+    let computerSelection = getComputerChoice();
+
+    let result = playRound(playerSelection, computerSelection);
+    let resultDiv = updateResult(result);
+
+
+    let playerScoreDiv = document.querySelector('.player-score')
+    let compScoreDiv = document.querySelector('.computer-score')
+
+    if (result.includes('Win') && resultDiv.textContent != 'You win!' && resultDiv.textContent != 'You lose!') {
+        let currentScoreArray = playerScoreDiv.textContent.split(':');
+        let newScore = +currentScoreArray[currentScoreArray.length - 1] + 1;
+        console.log(`PlayerScore = ${newScore}`);
+        playerScoreDiv.textContent = `Player Score: ${newScore}`;
+        if (newScore == 5) {
+            resultDiv.textContent = 'You win!';
+        }
+    }
+    if (result.includes('Lose') && resultDiv.textContent != 'You lose!' && resultDiv.textContent != 'You win!') {
+        let currentScoreArray = compScoreDiv.textContent.split(':');
+        let newScore = +currentScoreArray[currentScoreArray.length - 1] + 1;
+        console.log(`ComputerScore = ${newScore}`);
+        compScoreDiv.textContent = `Computer Score: ${newScore}`;
+        if (newScore == 5) {
+            resultDiv.textContent = 'You lose!';
+        }
+    }
+
 }
 
-game();
+
+const buttons = Array.from(document.querySelectorAll('.button'));
+buttons.forEach(button => button.addEventListener('click', handleOnClick));
+let playerScoreDiv = document.querySelector('.player-score')
+let compScoreDiv = document.querySelector('.computer-score')
+playerScoreDiv.textContent = 'Player Score: 0';
+compScoreDiv.textContent = 'Computer Score: 0';
+
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         let playerSelection = "rock";
+//         let computerSelection = getComputerChoice();
+//         console.log('playerSelection = ' + playerSelection);
+//         console.log(`computerSelection = ${computerSelection}`);
+//         console.log(playRound(playerSelection, computerSelection));
+//     }
+// }
+
+// game();
